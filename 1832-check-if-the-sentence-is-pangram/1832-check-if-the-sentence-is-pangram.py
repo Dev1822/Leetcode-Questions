57e1a3864156de41003d5ddad38c3823a1d660d8
Dev Patel <dev.d.patel.cg@gmail.com>
2026-02-07 19:09:35 +0530

    Time: 0 ms (100%), Space: 12.6 MB (23.92%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.py
@@ -4,17 +4,6 @@ class Solution(object):
         :type sentence: str
         :rtype: bool
         """
-        length=len(sentence)
-        for i in range(97,123):
-            x=chr(i)
-            check=False
-            for j in range(length):
-                if x==sentence[j]:
-                    check=True
-                    break
-            if not check:
-                return False
-
-        return True
+        return len(set(sentence))==26
         
         
