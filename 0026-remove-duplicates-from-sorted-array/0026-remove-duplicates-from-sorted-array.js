c38434ce8e4c2d0c881b47964c97c92f63334a93
Dev Patel <dev.d.patel.cg@gmail.com>
2026-02-07 14:50:09 +0530

    Time: 3 ms (15.58%), Space: 58.7 MB (20.44%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -2,8 +2,9 @@ var removeDuplicates = function(nums) {
     let obj = {};
     let count = 0;
     let arr = [];
+    let len=nums.length;
 
-    for (let i = 0; i < nums.length; i++) {
+    for (let i = 0; i < len; i++) {
         let x = nums[i];
 
         if (obj[x] === undefined) {
