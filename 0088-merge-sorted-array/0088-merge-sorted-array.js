983f5f69a53103f28ab103fa73927b5063c7d706
Dev Patel <dev.d.patel.cg@gmail.com>
2026-02-08 18:56:29 +0530

    Time: 0 ms (100%), Space: 54.4 MB (47.76%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -7,17 +7,7 @@
  */
 var merge = function(nums1, m, nums2, n) {
     for(let i=0;i<n;i++){
-        let found=false;
-        for(let j=0;j<m+i;j++){
-            if(nums1[j]>nums2[i]){
-                nums1.splice(j,0,nums2[i]);
-                found=true;
-                break;
-            }
-        }
-        if(!found){
-            nums1.splice(m+i,0,nums2[i]);
-        }
-        nums1.pop();
+        nums1[m+i]=nums2[i]
     }
+    nums1.sort((a,b)=>a-b)
 };
