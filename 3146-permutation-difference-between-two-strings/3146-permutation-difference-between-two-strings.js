cef8e91e8d7b3fb222edb4a0555a8161590639c1
Dev Patel <dev.d.patel.cg@gmail.com>
2026-02-10 23:01:36 +0530

    Time: 1 ms (79.66%), Space: 55.9 MB (79.66%) - LeetHub

---
+++ 3146-permutation-difference-between-two-strings/3146-permutation-difference-between-two-strings.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {number}
+ */
+var findPermutationDifference = function(s, t) {
+    let m=new Map()
+    let len=s.length
+    for(let i=0;i<len;i++){
+        m.set(s[i],i)
+    }
+    let pd=0;
+    let x;
+    for(let j=0;j<len;j++){
+        x=m.get(t[j])-j;
+        if(x<0) x=0-x
+        pd+=x
+    }
+    return pd
+};
