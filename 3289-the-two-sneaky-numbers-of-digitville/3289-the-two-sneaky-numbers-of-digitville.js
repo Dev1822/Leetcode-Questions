f3bea3f4d57683162b6b42e18708b4b5040f48db
Dev Patel <dev.d.patel.cg@gmail.com>
2026-02-10 17:37:01 +0530

    Time: 2 ms (59.55%), Space: 57.6 MB (20.97%) - LeetHub

--- 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.js
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.js
@@ -3,22 +3,11 @@
  * @return {number[]}
  */
 var getSneakyNumbers = function(nums) {
-    let m=new Map()
+    let s=new Set()
+    let res=[]
     for(let i of nums){
-        if(m.has(i)){
-            m.set(i,m.get(i)+1)
-        }
-        else{
-            m.set(i,1)
-        }
+        if(s.has(i)) res.push(i)
+        else s.add(i)
     }
-
-    res=[]
-    for(let [key,values] of m){
-        if(values===2){
-            res.push(key)
-        }
-    }
-
-    return res;
+    return res
 };
