c7c6745a88bc07114d758677d22bdb66ddf7c782
Dev Patel <dev.d.patel.cg@gmail.com>
2026-02-19 18:37:33 +0530

    Time: 18 ms (47.76%), Space: 12.4 MB (91.04%) - LeetHub

---
+++ 3274-check-if-two-chessboard-squares-have-the-same-color/3274-check-if-two-chessboard-squares-have-the-same-color.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def checkTwoChessboards(self, coordinate1, coordinate2):
+        """
+        :type coordinate1: str
+        :type coordinate2: str
+        :rtype: bool
+        """
+        first1=ord(coordinate1[0])%2==0
+        second1=int(coordinate1[1])%2==0
+        first2=ord(coordinate2[0])%2==0
+        second2=int(coordinate2[1])%2==0
+        return (first1!=second1)==(first2!=second2)
