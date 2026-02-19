e151169d775471c9213345625cf42871caf7aa0d
Dev Patel <dev.d.patel.cg@gmail.com>
2026-02-19 18:40:32 +0530

    Time: 15 ms (70.15%), Space: 12.5 MB (11.94%) - LeetHub

--- 3274-check-if-two-chessboard-squares-have-the-same-color/3274-check-if-two-chessboard-squares-have-the-same-color.py
+++ 3274-check-if-two-chessboard-squares-have-the-same-color/3274-check-if-two-chessboard-squares-have-the-same-color.py
@@ -5,8 +5,9 @@ class Solution(object):
         :type coordinate2: str
         :rtype: bool
         """
-        first1=ord(coordinate1[0])
-        second1=int(coordinate1[1])
-        first2=ord(coordinate2[0])
-        second2=int(coordinate2[1])
-        return ((first1+second1)%2==0)==((first2+second2)%2==0)
+        result=0
+        result+=ord(coordinate1[0])
+        result+=int(coordinate1[1])
+        result+=ord(coordinate2[0])
+        result+=int(coordinate2[1])
+        return result%2==0
