da64d83b959e1f6e0f25433f65d2d4ba66aa751a
Dev Patel <dev.d.patel.cg@gmail.com>
2026-02-20 19:08:35 +0530

    Time: 8 ms (45.8%), Space: 64 MB (59.58%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    x=String(x);
+    let i=0;
+    let j=x.length-1;
+    while(i<j){
+        if(x[i]!=x[j]) return false;
+        i++
+        j--
+    }
+    return true;
+};
