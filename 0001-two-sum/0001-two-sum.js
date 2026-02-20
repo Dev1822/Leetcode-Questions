d41f44c05099ee0394c3676dac73c8338354fc92
Dev Patel <dev.d.patel.cg@gmail.com>
2026-02-20 19:03:00 +0530

    Time: 46 ms (16.92%), Space: 53.1 MB (99.22%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -5,9 +5,10 @@
  */
 var twoSum = function(nums, target) {
     let len=nums.length;
+    let sum;
     for(let i=0;i<len;i++){
         for(let j=i+1;j<len;j++){
-            let sum=nums[i]+nums[j];
+            sum=nums[i]+nums[j];
             if(sum==target){
                 return [i,j];
             }
