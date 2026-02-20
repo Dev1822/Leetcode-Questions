86e6014950c39dd6dfc810ad021549c8b1875b47
Dev Patel <dev.d.patel.cg@gmail.com>
2026-02-20 18:56:13 +0530

    Time: 5 ms (13.34%), Space: 53.6 MB (96.59%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -4,16 +4,17 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
-    let intersection=[];
+    let intersection=new Set();
     let len1=nums1.length;
     let len2=nums2.length;
     for(let i=0;i<len1;i++){
         let x=nums1[i];
         for(let j=0;j<len2;j++){
-            if(nums2[j]===x && !(intersection.includes(x))){
-                intersection.push(x)
+            if(nums2[j]===x && !(intersection.has(x))){
+                intersection.add(x)
             }
         }
     }
+    intersection=[...intersection]
     return intersection;
 };
