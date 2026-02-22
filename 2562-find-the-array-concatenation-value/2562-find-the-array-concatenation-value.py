ee6b8461ea362b29fce90319d27d7a5b63bb93fe
Dev Patel <dev.d.patel.cg@gmail.com>
2026-02-22 21:31:26 +0530

    Time: 5 ms (51.43%), Space: 12.5 MB (55.24%) - LeetHub

--- 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.py
+++ 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.py
@@ -8,10 +8,10 @@ class Solution(object):
         i=0
         j=len(nums)-1
         while(i<=j):
-            if(i==j):
-                c+=nums[i]
-            else:
+            if(i!=j):
                 c+=int(str(nums[i])+str(nums[j]))
+            else:
+                c+=nums[i]
             i+=1
             j-=1
         return c
