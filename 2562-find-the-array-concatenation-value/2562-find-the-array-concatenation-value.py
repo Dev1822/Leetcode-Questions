021343bc930bad1d346900a262122fa494fc4e0a
Dev Patel <dev.d.patel.cg@gmail.com>
2026-02-22 21:29:12 +0530

    Time: 3 ms (85.71%), Space: 12.4 MB (87.62%) - LeetHub

--- 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.py
+++ 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.py
@@ -4,13 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        length=len(nums)
-        concatenation=0
-        while length>1:
-            x=str(nums[0])+str(nums[-1])
-            concatenation+=int(x)
-            nums=nums[1:length-1]
-            length=len(nums)
-        if length==1:
-            concatenation+=nums[0]
-        return concatenation
+        c=0
+        i=0
+        j=len(nums)-1
+        while(i<=j):
+            if(i==j):
+                c+=nums[i]
+            else:
+                c+=int(str(nums[i])+str(nums[j]))
+            i+=1
+            j-=1
+        return c
