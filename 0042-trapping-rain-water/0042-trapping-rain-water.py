895323f89a040e46cdd99d68e1c8f2d93df3a4a2
Dev Patel <dev.d.patel.cg@gmail.com>
2026-02-25 00:19:41 +0530

    Time: 8 ms (75.88%), Space: 13.5 MB (64.54%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def trap(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        left=0
+        right=len(height)-1
+        lm=0
+        rm=0
+        res=0
+        while left<right:
+            if height[left]<height[right]:
+                if height[left]>lm:
+                    lm=height[left]
+                else:
+                    res+=lm-height[left]
+                left+=1
+            else:
+                if height[right]>rm:
+                    rm=height[right]
+                else:
+                    res+=rm-height[right]
+                right-=1
+        return res
