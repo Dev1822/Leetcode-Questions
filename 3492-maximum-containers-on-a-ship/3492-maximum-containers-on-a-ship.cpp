2343a0a36fc68a48c8326d6d8eaf97f9d8d4f0a7
Dev Patel <dev.d.patel.cg@gmail.com>
2026-03-15 19:09:08 +0530

    Time: 0 ms (100%), Space: 8.5 MB (32.88%) - LeetHub

--- 3492-maximum-containers-on-a-ship/3492-maximum-containers-on-a-ship.cpp
+++ 3492-maximum-containers-on-a-ship/3492-maximum-containers-on-a-ship.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int maxContainers(int n, int w, int maxWeight) {
-        int total=n*n;
-        int i=1;
-        int weight=0;
-        int count=0;
-        while(i<=total){
-            if(weight+w>maxWeight){
-                return count;
-            }
-            weight+=w;
-            i++;
-            count++;
+        int a=n*n;
+        int b=maxWeight/w;
+        if(a<b){
+            return a;
         }
-        return count;
+        else{
+            return b;
+        }
     }
 };
