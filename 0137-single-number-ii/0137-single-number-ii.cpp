68850ba37f2d90641f81501dc7cb9ea0b6058f82
Dev Patel <dev.d.patel.cg@gmail.com>
2026-03-27 21:15:36 +0530

    Time: 3 ms (32.98%), Space: 13.9 MB (18.24%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int, int> obj;
+
+        for (int i : nums) {
+            obj[i] = obj[i] + 1;
+        }
+
+        for (auto& [key, value] : obj) {
+            if (value == 1) {
+                return key;
+            }
+        }
+
+        return -1;  
+    }
+};
