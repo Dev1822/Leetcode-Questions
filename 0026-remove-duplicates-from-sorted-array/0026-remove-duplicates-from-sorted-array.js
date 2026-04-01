8ebfd8a82a31adf1cf7b51e86dd7825b9b8b1f37
Dev Patel <dev.d.patel.cg@gmail.com>
2026-04-01 22:43:45 +0530

    Time: 1 ms (48.67%), Space: 58.2 MB (51.6%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -2,7 +2,8 @@ var removeDuplicates = function(nums) {
     if(nums.length===0) return 0;
     let i=0;
     let j=0;
-    while(j<nums.length){
+    let len=nums.length;
+    while(j<len){
         if(nums[j]!==nums[i]){
             i++;
             nums[i]=nums[j];
