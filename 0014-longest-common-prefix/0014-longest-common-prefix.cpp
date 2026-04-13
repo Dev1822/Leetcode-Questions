f2b9c787c467a990050e4c2c32cc5bbe8fc789db
Dev Patel <dev.d.patel.cg@gmail.com>
2026-04-13 16:02:20 +0530

    Time: 9 ms (4.53%), Space: 18.3 MB (5.02%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -3,29 +3,29 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         string res="";
         int length=strs.size();
+        bool found=true;
         vector <int> lens={};
         for(string i : strs){
             lens.push_back(i.size());
         }
         sort(lens.begin(),lens.end());
         int minlength=lens[0];
-        string first=strs[0];
         for(int i=0;i<minlength;i++){
-            char x=first[i];
-            bool found=false;
-            for(string j : strs){
-                if(j[i]!=x){
-                    found=true;
+            string x=res+strs[0][i];
+            for(string i : strs){
+                if(i.find(x)!=0){
+                    found=false;
                     break;
                 }
             }
-            if(!found){
-                res+=x;
+            if(found){
+                res=x;
             }
             else{
                 break;
             }
         }
         return res;
+        
     }
 };
