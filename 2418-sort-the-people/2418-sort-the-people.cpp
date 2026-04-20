544ad840c34cf2f245ae1e49c3ba534c96651d7a
Dev Patel <dev.d.patel.cg@gmail.com>
2026-04-20 14:46:47 +0530

    Time: 23 ms (20.95%), Space: 28.8 MB (34.27%) - LeetHub

---
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        map <int,string> obj={};
+        int length=names.size();
+        for(int i=0;i<length;i++){
+            obj.insert({heights[i],names[i]});
+        }
+        vector <string> res={};
+        for(auto& [key,value] : obj){
+            res.push_back(value);
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
