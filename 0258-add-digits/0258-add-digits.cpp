66960d5578a36d4855456b63869db16576bbe673
Dev Patel <dev.d.patel.cg@gmail.com>
2026-05-05 17:51:04 +0530

    Time: 3 ms (3.9%), Space: 8.7 MB (44.62%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        int res = 0;
+        while (num >= 10) {
+            res = 0;
+            while (num > 0) {
+                res += num % 10;
+                num /= 10;
+            }
+            num = res;
+        }
+        return num;
+    }
+};
