626d3f37acb7cc179542eb38e90b00fe365c04bc
Dev Patel <dev.d.patel.cg@gmail.com>
2026-05-11 16:48:27 +0530

    Time: 1 ms (57.07%), Space: 10.6 MB (13.45%) - LeetHub

---
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        vector <int> position={0,0};
+        for(char i : moves){
+            if(i=='U'){
+                position[1]++;
+            }
+            else if(i=='D'){
+                position[1]--;
+            }
+            else if(i=='L'){
+                position[0]--;
+            }
+            else if(i=='R'){
+                position[0]++;
+            }
+        }
+        return position == vector<int>{0,0};
+    }
+};
