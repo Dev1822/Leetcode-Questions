80764532cc68e337fd91b7296ba87ad99bc6ed19
Dev Patel <dev.d.patel.cg@gmail.com>
2026-05-11 17:06:22 +0530

    Time: 0 ms (100%), Space: 10.4 MB (45.62%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     bool judgeCircle(string moves) {
-        int x = 0, y = 0;
-
-        for(char i : moves) {
-            if(i == 'U') y++;
-            else if(i == 'D') y--;
-            else if(i == 'L') x--;
-            else if(i == 'R') x++;
+        vector <int> position={0,0};
+        for(char i : moves){
+            if(i=='U'){
+                position[1]++;
+            }
+            else if(i=='D'){
+                position[1]--;
+            }
+            else if(i=='L'){
+                position[0]--;
+            }
+            else if(i=='R'){
+                position[0]++;
+            }
         }
 
-        return x == 0 && y == 0;
+        return position==vector<int>{0,0};
     }
 };
