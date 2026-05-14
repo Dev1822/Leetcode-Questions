727abc97221f959cd457a4556d29331b5d265884
Dev Patel <dev.d.patel.cg@gmail.com>
2026-05-14 20:36:46 +0530

    Time: 0 ms (100%), Space: 13 MB (56.64%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
@@ -3,14 +3,14 @@ public:
     int mostWordsFound(vector<string>& sentences) {
         int max=0;
         for(auto& i : sentences){
-            int count=0;
+            int spaces=0;
             for(char j : i){
                 if(j==' '){
-                    count++;
+                    spaces++;
                 }
             }
-            if(count>max){
-                max=count;
+            if(spaces>max){
+                max=spaces;
             }
         }
         return max+1;
