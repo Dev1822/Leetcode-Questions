6111f063ef5fcb647c48b4019051ff309ccd276a
Dev Patel <dev.d.patel.cg@gmail.com>
2026-05-21 01:37:12 +0530

    Time: 0 ms (100%), Space: 36.3 MB (37.55%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.cpp
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
-        map <string,int> indices={{"type",0},{"color",1},{"name",2}};
-        int count=0;
-        for(auto& i : items){
-            if(i[indices[ruleKey]]==ruleValue){
+        int count = 0;
+
+        for(int i=0; i<items.size(); i++) {
+            if(ruleKey == "type" && ruleValue == items[i][0]) 
                 count++;
-            }
+            else if(ruleKey == "color" && ruleValue == items[i][1])
+                count++;
+            else if(ruleKey == "name" && ruleValue == items[i][2])
+                count++;    
         }
         return count;
     }
