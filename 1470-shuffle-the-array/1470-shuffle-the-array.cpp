23491a9a95ced962adc0fd4e079b0c000cd422be
Dev Patel <dev.d.patel.cg@gmail.com>
2026-05-21 01:25:49 +0530

    Time: 4 ms (64.71%), Space: 13.5 MB (16.46%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector <int> res(n*2,0);
-        int x=0;
-        int y=1;
+        vector<int> res;
         for(int i=0;i<n;i++){
-            res[x]=nums[i];
-            res[y]=nums[n+i];
-            x+=2;
-            y+=2;
+            res.push_back(nums[i]);
+            res.push_back(nums[i+n]);
         }
         return res;
     }
