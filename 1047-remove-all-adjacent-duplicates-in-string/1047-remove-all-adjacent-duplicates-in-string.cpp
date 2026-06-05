3316a47a5839a30dcba58562a572c0471a001bcd
Dev Patel <dev.d.patel.cg@gmail.com>
2026-06-05 18:40:23 +0530

    Time: 2 ms (84.23%), Space: 13.6 MB (69.92%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     string removeDuplicates(string s) {
         string res="";
-        for(char i : s){
+        for(int i : s){
             if(res.size()==0){
                 res.push_back(i);
             }
             else{
-                if(i==res[res.size()-1]){
+                if(res[res.size()-1]==i){
                     res.pop_back();
                 }
                 else{
