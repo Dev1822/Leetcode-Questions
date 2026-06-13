b7c49e2dabb4ced25a69ce432460128fdda0b5fc
Dev Patel <dev.d.patel.cg@gmail.com>
2026-06-13 11:28:49 +0530

    Time: 0 ms (100%), Space: 12 MB (84.91%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.cpp
+++ 1002-find-common-characters/1002-find-common-characters.cpp
@@ -6,12 +6,12 @@ public:
             string temp="";
             temp+=i;
             bool found=true;
-            for(int j = 1; j < words.size(); j++){
+            for(int j=1;j<words.size();j++){
                 if(!words[j].contains(i)){
                     found=false;
                     break;
                 }
-                words[j].erase(words[j].find(i), 1);
+                words[j].erase(words[j].find(i),1);
             }
             if(found){
                 res.push_back(temp);
