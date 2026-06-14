efe42e1efe21249af46e1b14d1807465e886a57c
Dev Patel <dev.d.patel.cg@gmail.com>
2026-06-14 17:46:54 +0530

    Time: 0 ms (100%), Space: 10.1 MB (12.48%) - LeetHub

--- 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.cpp
+++ 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     int maxFreqSum(string s) {
-        unordered_map<char, int> vowelCount;
-        unordered_map<char, int> consonantCount;
-        int maxVowel = 0;
-        int maxConsonant = 0;
-
-        for (char ch : s) {
-            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+        unordered_map <char,int> vowelCount;
+        unordered_map <char,int> consonantCount;
+        int maxVowel=0;
+        int maxConsonant=0;
+        for(char ch : s){
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
                 vowelCount[ch]++;
                 if(vowelCount[ch]>maxVowel){
                     maxVowel=vowelCount[ch];
                 }
-            } else {
+            }
+            else{
                 consonantCount[ch]++;
                 if(consonantCount[ch]>maxConsonant){
                     maxConsonant=consonantCount[ch];
                 }
             }
         }
-
-        return maxVowel + maxConsonant;
+        return maxVowel+maxConsonant;
     }
 };
