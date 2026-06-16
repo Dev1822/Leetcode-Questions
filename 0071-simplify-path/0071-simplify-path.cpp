fcdf26381a63a786c204b348eba28cc83ffb7bad
Dev Patel <dev.d.patel.cg@gmail.com>
2026-06-16 14:17:38 +0530

    Time: 0 ms (100%), Space: 11.7 MB (38.26%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string> st;
+        int i = 0;
+
+        while (i < path.size()) {
+            while (i < path.size() && path[i] == '/') {
+                i++;
+            }
+
+            string temp = "";
+            while (i < path.size() && path[i] != '/') {
+                temp += path[i];
+                i++;
+            }
+
+            if (temp == "" || temp == ".") {
+                continue;
+            }
+            else if (temp == "..") {
+                if (!st.empty()) {
+                    st.pop();
+                }
+            }
+            else {
+                st.push(temp);
+            }
+        }
+
+        vector<string> dirs;
+        while (!st.empty()) {
+            dirs.push_back(st.top());
+            st.pop();
+        }
+
+        reverse(dirs.begin(), dirs.end());
+
+        string res = "";
+        for (string &dir : dirs) {
+            res += "/" + dir;
+        }
+
+        return res.empty() ? "/" : res;
+    }
+};
