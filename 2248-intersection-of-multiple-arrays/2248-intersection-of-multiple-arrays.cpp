39da4fd5b9e61a7c6c533e5616d9553966f17371
Dev Patel <dev.d.patel.cg@gmail.com>
2026-06-17 18:43:21 +0530

    Time: 5 ms (39.87%), Space: 16.9 MB (51.72%) - LeetHub

--- 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.cpp
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<int> intersection(vector<vector<int>>& nums) {
-        map <int,int> count={};
+        map <int,int> freq={};
         for(auto& i : nums){
             for(int j : i){
-                count[j]++;
+                freq[j]++;
             }
         }
         int length=nums.size();
         vector <int> res={};
-        for(auto& [key,value] : count){
+        for(auto& [key,value] : freq){
             if(value==length){
                 res.push_back(key);
             }
