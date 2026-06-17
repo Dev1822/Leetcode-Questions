9e100c27399f5457106ed816fb182ab779fe5857
Dev Patel <dev.d.patel.cg@gmail.com>
2026-06-17 22:00:48 +0530

    Time: 0 ms (100%), Space: 8.2 MB (83.15%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int balancedStringSplit(string s) {
-        int l=0;
-        int r=0;
+        int L=0;
+        int R=0;
         int count=0;
         for(char i : s){
             if(i=='L'){
-                l+=1;
+                L+=1;
             }
             else{
-                r+=1;
+                R+=1;
             }
-            if(l==r){
+            if(L==R){
                 count+=1;
-                l=0;
-                r=0;
+                L=0;
+                R=0;
             }
         }
         return count;
