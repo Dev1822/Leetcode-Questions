fdf217734cfbfb44f6ae1654e5ce308e0e0431a8
Dev Patel <dev.d.patel.cg@gmail.com>
2026-06-23 18:45:53 +0530

    Time: 259 ms (93.56%), Space: 66.4 MB (64.19%) - LeetHub

--- 2886-change-data-type/2886-change-data-type.py
+++ 2886-change-data-type/2886-change-data-type.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
 def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
-    students["grade"] = pd.to_numeric(students["grade"], errors="coerce").astype("Int64")
-    return students
+    students["grade"] = students["grade"].astype(int)
+    return students 
