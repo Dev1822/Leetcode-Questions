5181db5860078fb3c01e2b17dfe18d9197e52364
Dev Patel <dev.d.patel.cg@gmail.com>
2026-07-17 15:09:08 +0530

    Time: 0 ms (100%), Space: 11.9 MB (85.02%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -3,12 +3,7 @@ public:
     bool uniqueOccurrences(vector<int>& arr) {
         unordered_map <int,int> obj={};
         for(int i : arr){
-            if(obj[i]){
-                obj[i]=obj[i]+1;
-            }
-            else{
-                obj[i]=1;
-            }
+            obj[i]++;
         }
         set <int> freq={};
         for(auto& [key,value] : obj){
