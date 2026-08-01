e10a77cedc384248292300a88be83c7726c4c30d
Dev Patel <dev.d.patel.cg@gmail.com>
2026-08-01 11:49:18 +0530

    Time: 658 ms (81.05%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -1,13 +1,13 @@
 # Write your MySQL query statement below
 SELECT 
-d.name as "Department",
-e.name as "Employee",
-e.salary as "Salary"
+D.NAME as "Department",
+E.NAME as "Employee",
+E.SALARY as "Salary"
 from Employee e
-INNER JOIN Department d
+LEFT JOIN Department d
 on e.departmentId=d.id
 where CONCAT(e.departmentId,e.salary) in (
     SELECT CONCAT(departmentId,MAX(salary))
-    FROM Employee
-    group by departmentId
+    FROM EMPLOYEE
+    GROUP BY DEPARTMENTId
 );
